d67a7c8e41b35b261ce8f1e9e93c1b71c87dd04a
Dan Kirkwood <dangogh@gmail.com>
2012-11-11 22:34:49 -0700

    empty space and rock

--- space.go
+++ space.go
@@ -2,8 +2,8 @@ package oceansim
 
 // Space interface -- each type of space must implement Act() and React()
 type Space interface {
-  Act()
-  React(Space) bool
+	Act()
+	React(Space) (blocks, reacts bool)
 }
 
 ////////////////////////////////////////////////////////////////
@@ -15,9 +15,9 @@ func (x Empty) Act() {
 	// do nothing
 }
 
-func (x Empty) React(Space) bool {
-	// do not block
-	return false
+func (x Empty) React(Space) (blocks, reacts bool) {
+	// do not block or react
+	return false, false
 }
 
 ////////////////////////////////////////////////////////////////
@@ -29,7 +29,77 @@ func (x Rock) Act() {
 	// do nothing
 }
 
-func (x Rock) React(Space) bool {
+func (x Rock) React(Space) (blocks, reacts bool) {
 	// block
-	return true
+	return true, false
+}
+
+
+// Critter interface -- is a space that moves and reproduces
+type Critter interface {
+	// include Space interface
+	Act()
+	React(Space) (blocks, reacts bool)
+	Move() Space
+	Reproduce() Space
+
+	// private methods
+	endCycle() bool
+	reproduce() bool
+	move() bool
+}
+
+
+// Plankton is simplest form of Critter -
+//  no size, but reproduces at regular intervals
+type Plankton struct {
+	// private data members
+	age uint
+	timeSinceReproduced uint
+	timeSinceAte uint
+
+}
+
+// Public interface
+func (x Plankton) Act() {
+	if x.endCycle() {
+		x.reproduce()
+	} else {
+		x.move()
+	}
+}
+
+func (x Plankton) reproduce() bool {
+	return false
+}
+
+func (x Plankton) move() bool {
+	return false
+}
+
+// Public
+//  React to another invading my space
+func (x Plankton) React(other Space) (blocks, reacts bool) {
+	return true, true
+}
+
+// Private
+//  is it time to reproduce?
+func (x Plankton) endCycle() bool {
+	return x.age > 0 && x.age % x.timeSinceReproduced == 0
+}
+
+// Fish is a Critter with size that eats lesser Critters
+//  including Plankton and smaller Fish.
+// It reproduces at intervals depending on size.
+// Size is dependent on age and diet
+type Fish struct {
+	// include Critter interface
+	Critter
+}
+
+// Shark is a Fish. It will eat any size Fish and smaller
+// Sharks.  It will attack a larger Shark if starving.
+type Shark struct {
+	Fish
 }
--- space_test.go
+++ space_test.go
@@ -5,15 +5,32 @@ import (
 )
 
 func TestSpaces(t *testing.T) {
-	s := Empty{}
-	r := Rock{}
-	s.Act()
-	// space does not block
-	if s.React(r) {
+	s0,s1 := Empty{}, Empty{}
+	r0,r1 := Rock{}, Rock{}
+
+	////////////////////
+	// Empty Space
+	s0.Act()
+	blocks, reacts := s0.React(s1)
+
+	// does not block
+	if blocks {
 		t.Fail()
 	}
-	// rock blocks
-	if ! r.React(s) {
+	// does not react
+	if reacts {
+		t.Fail()
+	}
+
+	////////////////////
+	// Rock
+	blocks, reacts = r0.React(r1)
+	// blocks
+	if ! blocks {
+		t.Fail()
+	}
+	// does not react
+	if reacts {
 		t.Fail()
 	}
 }
